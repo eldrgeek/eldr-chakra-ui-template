ea27ae75c29b1a3580f26f52472788bc190280d4
Mike Wolf <mw@mike-wolf.com>
2021-03-18 16:35:23 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Container } from '@chakra-ui/react';
+
+export default function App() {
+	return (
+		<Container h="100vh" d="flex" alignItems="center" justifyContent="center">
+			stuff
+		</Container>
+	);
+}
